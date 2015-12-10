1f1369af697c78a1c38f10c19a8673b552fc0646
susinda <susinda@wso2.com>
2015-12-10 09:41:56 +0530

    add datamapper working code from 380 commit  ea6b0bb9d702875b75213f0cd01931aee8ca42ef

---
+++ datamapper-engine/org.wso2.datamapper.engine/src/main/java/org/wso2/datamapper/engine/inputAdapters/XmlInputReader.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2005,2013 WSO2, Inc. http://www.wso2.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.wso2.datamapper.engine.inputAdapters;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Field;
+import org.apache.avro.Schema.Type;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericData.Array;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.axiom.om.OMElement;
+
+
+public class XmlInputReader implements InputDataReaderAdapter {
+
+	private OMElement body; // Soap Message body
+ 
+	/**
+	 * @param msg - Soap Envelop
+	 * @throws IOException
+	 */
+	public void setInputMsg(OMElement msg) {
+		this.body = msg.getFirstElement().getFirstElement();
+	}
+
+	public GenericRecord getInputRecord(Schema input) {
+
+		@SuppressWarnings("unchecked")
+		GenericRecord inputRecord = getChild(input, this.body.getChildElements());
+		return inputRecord;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	private GenericRecord getChild(Schema schema, Iterator<OMElement> iter) {
+
+		GenericRecord record = new GenericData.Record(schema);
+		ConcurrentMap<String, Collection<Object>> arrMap = new ConcurrentHashMap<String, Collection<Object>>();
+
+		while (iter.hasNext()) {
+			OMElement element = iter.next();
+			String localName = element.getLocalName();
+			Field field = schema.getField(localName);
+			if (field != null) {
+				if (field.schema().getType().equals(Type.ARRAY)) {
+					Collection<Object> arr = arrMap.get(localName);
+					if (arr == null) {
+						arr = new ArrayList<Object>();
+						arrMap.put(localName, arr);
+					}
+					if (field.schema().getElementType().getType().equals(Type.RECORD)) {
+						Iterator childElements = element.getChildElements();
+						GenericRecord child = getChild(field.schema().getElementType(),
+								childElements);
+						arr.add(child);
+					} else if (field.schema().getElementType().getType().equals(Type.ARRAY)) {
+						// not supports yet!
+					} else { // !(ARRAY||RECORD) != primitive type
+						arr.add(element.getText());
+					}
+				} else if (field.schema().getType().equals(Type.RECORD)) {
+					Iterator childElements = element.getChildElements();
+					GenericRecord child = getChild(field.schema(), childElements);
+					record.put(localName, child);
+				} else {
+					record.put(localName, element.getText());
+					// TODO: fix for other types too... !(ARRAY||RECORD) !=
+					// primitive type
+				}
+			} else {
+				// TODO: log unrecognized element
+			}
+		}
+
+		for (Entry<String, Collection<Object>> arrEntry : arrMap.entrySet()) {
+			String key = arrEntry.getKey();
+			Object object = record.get(key);
+			Array<Object> childArray = null;
+			Collection<Object> value = arrEntry.getValue();
+			if (object == null) {
+				childArray = new GenericData.Array<Object>(value.size(), schema.getField(key)
+						.schema());
+			} else {
+				childArray = (Array<Object>) object;
+			}
+			for (Object obj : value) {
+				childArray.add(obj);
+			}
+			record.put(key, childArray);
+
+		}
+
+		return record;
+	}
+}
---
+++ datamapper-engine/org.wso2.datamapper.engine/src/main/java/org/wso2/datamapper/engine/sample/MapperMain.java
@@ -0,0 +1,88 @@
+package org.wso2.datamapper.engine.sample;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMXMLBuilderFactory;
+import org.apache.axiom.om.OMXMLParserWrapper;
+import org.json.JSONException;
+import org.wso2.datamapper.engine.core.MappingHandler;
+import org.wso2.datamapper.engine.core.MappingResourceLoader;
+import org.wso2.datamapper.engine.core.writer.DummyEncoder;
+import org.wso2.datamapper.engine.core.writer.WriterRegistry;
+import org.wso2.datamapper.engine.inputAdapters.CsvInputReader;
+/**
+ * This is a Test class will be removed in the future
+ *
+ */
+public class MapperMain {
+
+	public static void main(String[] args) throws FileNotFoundException, JSONException {
+
+		InputStream inStream = new FileInputStream(new File("./resources/jiraInput.xml"));
+		InputStream config = new FileInputStream(new File("./resources/mappingConfigJira.js"));
+		InputStream inputSchema = new FileInputStream(new File("./resources/csvInput.avsc"));
+		InputStream outputSchema = new FileInputStream(new File("./resources/jiraoutput.avsc"));
+		
+		try {
+	
+			
+			OMXMLParserWrapper builder = OMXMLBuilderFactory.createOMBuilder(inStream);
+		    OMElement documentElement = builder.getDocumentElement(); 
+		    
+		    MappingResourceLoader configModel = new MappingResourceLoader(inputSchema,outputSchema, config);
+		    GenericRecord result = MappingHandler.doMap(documentElement,configModel,new CsvInputReader());			
+		 
+			
+	         ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
+			
+			//CSV : text/csv
+			//XML : application/xml
+		    
+		  /*  DatumWriter<GenericRecord> writer = WriterRegistry.getInstance().get("text/csv").newInstance(); 
+		    writer.setSchema(result.getSchema());
+		    Encoder encoder =  new DummyEncoder(baos);
+		 
+		    writer.write(result, encoder);
+		    encoder.flush();
+
+		    //Print CSV
+		    System.out.println("-- CSV -- \n");
+			System.out.println(baos.toString());*/
+			
+			
+			baos = new ByteArrayOutputStream();
+			Encoder encoder =  new DummyEncoder(baos);
+		    DatumWriter<GenericRecord>  writer = WriterRegistry.getInstance().get("application/xml").newInstance(); 
+		    writer.setSchema(result.getSchema());
+		    encoder =  new DummyEncoder(baos);
+		 
+		    writer.write(result, encoder);
+		    encoder.flush();
+
+		    //Print XML
+		    System.out.println("\n\n-- XML -- \n");
+			System.out.println(baos.toString());
+			
+			inStream.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+	}
+
+}
